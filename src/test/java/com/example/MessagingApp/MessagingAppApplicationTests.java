fd1a347e99d3ce696559a9eba1e620c16be7fe02
thepredator07 <khubaibrahman07@gmail.com>
2023-08-09 04:23:19 +0530

    First Commit

---
+++ src/test/java/com/example/MessagingApp/MessagingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MessagingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
