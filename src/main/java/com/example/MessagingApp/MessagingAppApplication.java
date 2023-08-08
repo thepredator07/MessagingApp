d7c4499ec37990eb1d743bb4b75dd2dd28dbac6e
thepredator07 <khubaibrahman07@gmail.com>
2023-08-09 04:07:53 +0530

    First Commit

---
+++ src/main/java/com/example/MessagingApp/MessagingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.MessagingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingAppApplication.class, args);
+	}
+
+}
